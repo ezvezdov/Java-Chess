f55e8d46aeb36579cf90e01b4795fa430c105674
Zviazdou, Yauheni <zviazyau@fel.cvut.cz>
2022-05-14 18:11:45 +0200

    module-info changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,12 @@ module cz.cvut.fel.pjv {
     requires javafx.controls;
     exports cz.cvut.fel.pjv;
     exports cz.cvut.fel.pjv.view;
+    requires javafx.fxml;
+
+    requires javafx.base;
+    requires javafx.graphics;
+
+    opens cz.cvut.fel.pjv to javafx.fxml;
+    opens cz.cvut.fel.pjv.view to javafx.fxml;
 }
+
